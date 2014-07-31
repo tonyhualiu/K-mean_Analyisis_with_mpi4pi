801770b3eaad498a9b41e4168e6f869f2b86ae3d
Tony Liu <tonyhualiu1912@gmail.com>
2014-07-31 18:48:11 -0400

    finish bash driver coding

--- run_parrellel.sh
+++ run_parrellel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 usage(){
-	echo "usage: -c <number of cluster> -p <number of points per cluster> -f <data file path> -t <type of data['point' or 'DNA']> -v [max value] -h [threshold] -l <logpath>"
+	echo "usage: \n\t-c <number of cluster>\n\t-n <number of processor>\n\t-m <path of machine file>\n\t-p <number of points per cluster>\n\t-f <data file path>\n\t-t <type of data['point' or 'DNA']>\n\t-v <max value of point or length of DNA>\n\t-h <threshold>\n\t-l <logpath>\nall must be speficified."
 };
 
 c=
+n=
+m=
 p=
 f=
 t=
@@ -14,10 +16,16 @@ l=
 point="point"
 dna="DNA"
 
-while getopts ":c:p:f:t:v:h:l:" opt; do
+while getopts ":c:n:m:p:f:t:v:h:l:" opt; do
 	case $opt in
 		c)
 			c=$OPTARG
+			;;
+		n)
+			n=$OPTARG
+			;;
+		m)
+			m=$OPTARG
 			;;
 		p)
 			p=$OPTARG
@@ -44,7 +52,7 @@ while getopts ":c:p:f:t:v:h:l:" opt; do
 	esac
 done
 
-if [ -z "$c" ] || [ -z "$p" ] || [ -z "$f" ] || [ -z "$t" ] || [ -z "$v" ] || [ -z "$h" ] || [ -z "$l" ];then
+if [ -z "$c" ] || [ -z "$n" ] || [ -z "$m" ] || [ -z "$p" ] || [ -z "$f" ] || [ -z "$t" ] || [ -z "$v" ] || [ -z "$h" ] || [ -z "$l" ];then
     usage
 	exit 1
 fi
@@ -55,7 +63,10 @@ if [ "$t" != "$point" ] && [ "$t" != "$dna" ];then
 	exit 1
 fi
 
+if [ "$t" = "$point" ];then	
+	python generaterawdata.py -c $c -p $p -o $f -v $v
+else
+	python generatorawdata.py -c $c -p $p -o $f -v $v
+fi
 
-echo "here"
-#python generaterawdata.py
-#python src/ParallelMachine.py
+mpiexec -n $n -machinefile $m python src/ParallelMachine.py $c $f $t $h $l 
